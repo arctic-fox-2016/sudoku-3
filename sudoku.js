082629ac1ab42cc74024da210903b75dcaf1091f
Ivan Gerard & Tevin <pair+ivangerard.tevinstein@hacktiv8.com>
2016-09-05 21:02:07 +0700

    Sudoku3 done

--- sudoku.js
+++ sudoku.js
@@ -1,1 +1,19 @@
 "use strict"
+
+var sudokuboard = [];
+
+//
+// sudoku_2_chooseoal(7)
+// solve();
+var fs = require('fs')
+var soal = fs.readFileSync('set-01_sample.unsolved.txt')
+  .toString()
+  .split("\n")[0]
+
+var SudokuSolver = require('sudoku-solver');
+
+SudokuSolver.solve({
+    problem: soal
+  , style: true
+  , initial: true
+}).printSolution();
