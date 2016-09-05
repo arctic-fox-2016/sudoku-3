f3a28f78ed2b3cf42c9f507839006e68f34a354e
Aji Lantang Mardika & Sahbana <pair+ajilantang.sahbanagold@hacktiv8.com>
2016-09-05 21:28:08 +0700

    pake nps solver sih

--- sudoku.js
+++ sudoku.js
@@ -1,1 +1,13 @@
 "use strict"
+var fs = require('fs')
+var soal = fs.readFileSync('set-01_sample.unsolved.txt')
+  .toString()
+  .split("\n")[14]
+
+var SudokuSolver = require("sudoku-solver");
+
+SudokuSolver.solve({
+  problem: soal,
+  style: true,
+  initial: true
+}).printSolution();
