4b76989b619bffd0e91434cd69e1c2f89df2e564
Ari Adiprana & Andrew Tandiawan <pair+ariadiprana.atandiawan@hacktiv8.com>
2016-09-05 12:15:03 +0700

    Sudoku Solver

--- sudoku.js
+++ sudoku.js
@@ -1,1 +1,14 @@
 "use strict"
+
+var fs = require('fs')
+var SudokuSolver = require('sudoku-solver');
+var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
+  .toString()
+  .split("\n")[0]
+
+console.log(board_string)
+SudokuSolver.solve({
+    problem: board_string
+  , style: true
+  , initial: true
+}).printSolution();
