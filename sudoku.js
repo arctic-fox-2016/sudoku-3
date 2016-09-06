ffeeae69f7e93394594ba98140faecbb5e2a853e
Ahmad Ahmadi & Ryan Andhaka <pair+ahmadahmadi14.ryanandhaka@hacktiv8.com>
2016-09-06 08:50:47 +0700

    Ahmad Ahmadi & Ryan Andhaka

--- sudoku.js
+++ sudoku.js
@@ -1,1 +1,19 @@
 "use strict"
+
+
+var sudokuboard = [];
+
+
+var fs = require('fs')
+var soal = fs.readFileSync('set-01_sample.unsolved.txt')
+  .toString()
+  .split("\n")[1]
+
+
+var SudokuSolver = require('sudoku-solver');
+
+SudokuSolver.solve({
+    problem: soal
+  , style: true
+  , initial: true
+}).printSolution();
